721bf6291896f4a27641320d86bb944ebc45edda
admin <862975466@qq.com>
2020-04-16 00:55:18 +0800

    删除配置配置

--- webpack.config.js
+++ webpack.config.js
@@ -38,24 +38,4 @@ module.exports = {
      */
   })
   ],
-  devServer:{
-    contentBase: path.join(__dirname,"dist"),//用于静态文件的目录，不设置默认为当前根目录
-    // contentBase:[path.join(__dirname,'public'),path.join(__dirname,'assets')],//支持多路径
-    // publicPath:"/assets", 服务器地址:http://localhost:8080 ,output file:http://localhost:8080/assets/bundle.js
-    //compress:true,//gzip压缩
-    //headers:{"X-Custom-Foo":"bar"},
-    hot:true,//是否启用热更新
-    port:8090,
-    historyApiFallback:true,//html5接口,设置为true，所有路径均转到index.html
-    inline:true,//是否实时刷新，即代码有更改，自动刷新浏览器
-    stats:{colors:true},//显示bundle文件信息，不同类型的信息用不同的颜色显示
-    /*
-    proxy:{     //服务器代理配置        "/api":{  //相对路径已/api打头，将会触发代理
-            target:"http://localhost:3000", //代理地址
-            pathRewrite:{"^/api":""}, //路径替换
-            secure:false //跨域
-        }
-    }
-    */
-  },
 }
